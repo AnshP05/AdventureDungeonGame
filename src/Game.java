e7cac3669563c19def08adfaa9e042a9dbc7f73f
AwesomeAnsh69 <23parasharansh@gmail.com>
2025-03-05 12:00:02 -0800

    Creation of the scanner and gamestate variables(enemies, health, potions etc) as well as importing the nessecary java packages

---
+++ src/Game.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+    //system objects
+    Scanner in = new Scanner(System.in);
+
+    Random R = new Random();
+
+    //game variables
+    String [] enemies = {"Skeleton", "Zombie", "Warrior", "Assasian"};
+    int maxEnemyHealth = 70;
+    int enemyAttackDamage = 30;
+
+    //player variable
+    int health = 100;
+    int attackdamage = 25;
+    int numHealthPotions = 4;
+    int healthPotionHealAmount = 30;
+    int healthPotionDropChance = 50; // percentage
+
+    boolean running = true;;
+    System.out.println("WELCOME TO THE DUNGEON");
+
+
+        
+  }
+}
